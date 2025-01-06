4b964d3a73bb2758160220ebd9d21a4cfd615a73
Morgane <m.chapeliere@mailistec.fr>
2025-01-06 16:29:32 +0100

    adding .env

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const uniqid = require('uniqid');
-const cloudinary = require('cloudinary').v2;
-const fs = require('fs');
+const uniqid = require("uniqid");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
+router.post("/upload", async (req, res) => {
+  const photoPath = `./tmp/${uniqid()}.jpg`;
+  const resultMove = await req.files.photoFromFront.mv(photoPath);
 
-router.post('/upload', async (req, res) => {
-    const photoPath = `./tmp/${uniqid()}.jpg`;
-    const resultMove = await req.files.photoFromFront.mv(photoPath);
-   
-    if(!resultMove) {
-        const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+  if (!resultMove) {
+    const resultCloudinary = await cloudinary.uploader.upload(photoPath);
 
-        fs.unlinkSync(photoPath);
-        console.log(resultCloudinary.secure_url)
-    res.json({ result: true, url: resultCloudinary.secure_url }); 
-    } else {
-      res.json({ result: false, error: resultCopy });
-    }
-   });
-
+    fs.unlinkSync(photoPath);
+    console.log(resultCloudinary.secure_url);
+    res.json({ result: true, url: resultCloudinary.secure_url });
+  } else {
+    res.json({ result: false, error: resultCopy });
+  }
+});
 
 module.exports = router;
